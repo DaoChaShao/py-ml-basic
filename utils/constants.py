bdc5c20b92b3710c34d147dc945956e9625f06ac
DaoChaShao <27882272@qq.com>
2026-09-18 23:54:58 +1200

    feat(utils): add constants module with WIDTH configuration
    - Create constants.py module to store application constants
    - Add WIDTH constant set to 64 pixels
    - Include standard Python file headers and metadata
    - Add main guard for potential standalone execution

---
+++ utils/constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2026/9/18 22:23
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   constants.py
+# @Desc     :   
+
+WIDTH: int = 64
+
+if __name__ == "__main__":
+    pass
