0731277c4edcc50f73c55839c80a6b7f7c166ac4
DaoChaShao <27882272@qq.com>
2026-09-25 21:46:28 +1200

    feat(ml): add boosting estimators module with GBDT implementation
    - Create new boosting subpackage under ml/estimators
    - Add GBDTree implementation in gbdt module
    - Include proper module initialization with __all__ export
    - Add comprehensive docstring documentation
    - Set up author and version metadata
    - Configure module imports for public API access

---
+++ utils/ml/estimators/boosting/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2026/9/25 21:34
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   __init__.py.py
+# @Desc     :
+
+"""
+****************************************************************
+Machine Learning Module - Boosting Estimators
+----------------------------------------------------------------
+This subpackage provides ensemble learning algorithms based on
+the Boosting strategy, including Gradient Boosting estimators and so on.
+****************************************************************
+"""
+
+__author__ = "Shawn Yu"
+__version__ = "0.1.0"
+
+from .gbdt import GBDTree
+
+__all__ = [
+    "GBDTree",
+]
