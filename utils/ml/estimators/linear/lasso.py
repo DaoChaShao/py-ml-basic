487b0ae9957998efc2f1b9709f41c1bcfc0661c9
DaoChaShao <27882272@qq.com>
2026-09-27 00:34:50 +1200

    refactor(utils): move lasso estimator to linear module and update import path
    - Moved lasso.py from utils/ml/estimators/ to utils/ml/estimators/linear/
    - Updated import statement from relative import to absolute import path
    - Changed from .base import Base to utils.ml.estimators.base import Base
    - Maintained same class structure and functionality in LassoRegressor

--- utils/ml/estimators/linear/lasso.py
+++ utils/ml/estimators/linear/lasso.py
@@ -14,7 +14,7 @@ from pydantic import Field, validate_call
 from sklearn.base import BaseEstimator
 from sklearn.linear_model import Lasso
 
-from .base import Base
+from utils.ml.estimators.base import Base
 
 
 class LassoRegressor(Base):
