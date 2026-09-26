be2e96321c2942aa13569940e39fc50fb340a172
DaoChaShao <27882272@qq.com>
2026-09-27 00:34:37 +1200

    refactor(utils): move enet estimator to linear module and update import path
    - Moved enet.py from utils/ml/estimators/ to utils/ml/estimators/linear/
    - Updated import statement from relative to absolute path for Base class
    - Maintained same ElasticNetRegressor functionality with proper module structure

--- utils/ml/estimators/linear/enet.py
+++ utils/ml/estimators/linear/enet.py
@@ -14,7 +14,7 @@ from pydantic import Field, validate_call
 from sklearn.base import BaseEstimator
 from sklearn.linear_model import ElasticNet
 
-from .base import Base
+from utils.ml.estimators.base import Base
 
 
 class ElasticNetRegressor(Base):
