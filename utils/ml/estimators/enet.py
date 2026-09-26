49e89d327c135de3a4c3eb184dc4c21410cba6e1
DaoChaShao <27882272@qq.com>
2026-09-26 22:21:44 +1200

    refactor(utils): reorder imports in elastic net estimator
    - Reordered typing imports to follow alphabetical convention
    - Moved override import after Self import for consistency

--- utils/ml/estimators/enet.py
+++ utils/ml/estimators/enet.py
@@ -6,7 +6,7 @@
 # @File     :   elastic.py
 # @Desc     :   ElasticNet (L1 + L2 Regularised) Regression Estimator Wrapper.
 
-from typing import Any, override, Self
+from typing import Any, Self, override
 
 from access_modifiers import protectedmethod
 from pandas import DataFrame, Series
